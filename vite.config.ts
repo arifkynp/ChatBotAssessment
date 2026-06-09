d73c7f3865dd5ac549713ee2606a3aaba15a468c
arifky.prasetya <arifky.prasetya@brank.as>
2026-06-09 08:58:04 +0700

    bugfix(ANP-0004): Fix build issue

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig(({ mode }) => ({
     alias: {
       "~": path.resolve(__dirname, "./src"),
     },
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
   },
   plugins: [
     tsconfigPaths(),
